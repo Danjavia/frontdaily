1139a02ebf39e4c4938ae80e56b3c11a1f46a752
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-02 22:29:44 -0500

    state

--- src/App.js
+++ src/App.js
@@ -22,6 +22,17 @@ class App extends Component {
         lng: position.coords.longitude
       });
     });
+
+    navigator.mediaDevices.enumerateDevices()
+      .then(function(devices) {
+        devices.forEach(function(device) {
+          console.log(device.kind + ": " + device.label +
+            " id = " + device.deviceId);
+        });
+      })
+      .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+      });
   }
 
   screenshot = () => {
@@ -36,8 +47,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Frontend Daily</h2>
         </div>
-        { this.state.screenshot ? <img src={this.state.screenshot} /> : null }
-        {!this.state.screenshot ? <Webcam audio={false} width="320" height="300" ref="webcam" />: null}
+        { this.state.screenshot ? <img src={this.state.screenshot} role="presentation" /> : null }
+        {!this.state.screenshot ? <Webcam audio={false} width="320" height="300" ref="webcam" role="presentation" />: null}
         {this.state.lat ? <div className="geo">
           <strong>latitude</strong>: {this.state.lat} -
           <strong>longitude:</strong> {this.state.lng}
