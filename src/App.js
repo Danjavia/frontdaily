43d102e455c556196bef711b7a8c01636494a70e
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-02 22:32:49 -0500

    state

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     navigator.mediaDevices.enumerateDevices()
       .then(function(devices) {
         devices.forEach(function(device) {
-          console.log(device.kind + ": " + device.label +
+          alert(device.kind + ": " + device.label +
             " id = " + device.deviceId);
         });
       })
